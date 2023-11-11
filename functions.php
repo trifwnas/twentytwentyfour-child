42241a47dab0cb80c99abbe0eefb2beaa351db6b
trifwnas <trif.tz@gmail.com>
2023-11-11 14:16:13 +0100

    Init

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+// enqueue parent styles
+
+function ns_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+
+add_action( 'wp_enqueue_scripts', 'ns_enqueue_styles' );
